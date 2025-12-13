48af01a03c1fd9965098928b5ab4cecfb8cc2cf1
Jan Lajara <janlajara@gmail.com>
2025-12-13 17:21:36 +0800

    double quote in fstring bug fix

--- inventory/admin.py
+++ inventory/admin.py
@@ -302,7 +302,7 @@ class ItemAdmin(ModelAdmin):
                     
                     messages.success(request, f"'{item.name}' withdrawn successfully")
                     return redirect(
-                        f"{reverse_lazy("admin:inventory_item_change", args=[object_id])}#movements"
+                        f"{reverse_lazy('admin:inventory_item_change', args=[object_id])}#movements"
                     )
                 except ValidationError as e:
                     messages.error(request, f"{e}")
