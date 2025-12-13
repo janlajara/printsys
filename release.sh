300fd4e244656dbac759399775a997cc92727a87
Jan Lajara <janlajara@gmail.com>
2025-12-13 16:47:59 +0800

    remove fixture loaddata

--- release.sh
+++ release.sh
@@ -2,6 +2,3 @@
 set -e
 python manage.py migrate
 python manage.py makesuperuser
-python manage.py loaddata fixtures/stockmovementpurpose.json
-python manage.py loaddata fixtures/itemcategory.json
-python manage.py loaddata fixtures/item.json
